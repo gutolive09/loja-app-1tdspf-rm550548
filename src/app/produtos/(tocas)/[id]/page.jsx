1c5356fe2b8c0bb522a259f9c86d736f9bb8ed99
gutolive09 <gutolive01@gmail.com>
2023-10-18 11:17:11 -0300

    Rotas dinâmicas e agrupadas adicionadas.

---
+++ src/app/produtos/(tocas)/[id]/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ProdutoId({params}) {
+  return (
+    <>
+    <h1>Produto ID</h1>
+    <p>Id: {params.id}</p>
+    </>
+  )
+}
---
+++ src/app/produtos/mosfet/[slug]/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function produtoSlug({params}) {
+  return (
+    <>
+        <p>O valor do atributo slug é {params.slug}</p>
+    </>
+  )
+}
