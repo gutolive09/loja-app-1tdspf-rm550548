c4dc77574ae411f2e322b9aa560bc0ef180abca5
gutolive09 <gutolive01@gmail.com>
2023-10-18 11:56:22 -0300

    estilos tailwind adicionados

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,11 @@
 import Image from 'next/image'
-import Link from 'next/link'
 
 export default function Home() {
   return (
     <>
       <h1>HOME</h1>
 
-      <div>
-        <h2>MEUS PRODUTOS</h2>
-        <ul>
-          <li><Link href="/produtos/tenis">Tênis</Link></li>
-          <li><Link href="/produtos/meia">Meia</Link></li>
-          <li><Link href="/produtos/camisa">Camisa</Link></li>
-          <li><Link href="/produtos/calca">Calça</Link></li>
-        </ul>
-      </div>
+  
 
       <figure>
         <Image
