8b11bee48e530d02278290d246f599d6cfbf6681
gutolive09 <gutolive01@gmail.com>
2023-10-26 11:52:40 -0300

    criado pagina para exibir eletrônicos por ID.

--- src/app/consumo/eletro-view/page.jsx
+++ src/app/consumo/eletro-view/page.jsx
@@ -1,37 +1,44 @@
 import Image from "next/image";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function EletroView() {
+  let eletros;
 
-    let eletros
-
-    try {
-        const response = await fetch('http://localhost:3000/api/eletronicos')
-        eletros = await response.json()
-        console.log(eletros)
-    } catch (error) {
-        console.log(error)
-        redirect('/error')
-    }
+  try {
+    const response = await fetch("http://localhost:3000/api/eletronicos/0");
+    eletros = await response.json();
+    console.log(eletros);
+  } catch (error) {
+    console.log(error);
+    redirect("/error");
+  }
 
   return (
     <div className="eletro-view">
-        <h1>Consumo de eletrodomésticos</h1>
-        <div>
-            <ul>
-                {
-                    eletros.map((item)=>(
-                        <li key={item.id}>
-                            <h2>{item.nome}</h2>
-                            <figure>
-                                <Image src={item.img} alt={item.desc} width={300} height={300}/>
-                                <figcaption>{item.desc} - <span>R${item.preco}</span></figcaption>
-                            </figure>
-                        </li>
-                    ))
-                }
-            </ul>
-        </div>
+      <h1>Consumo de eletrodomésticos</h1>
+      <div>
+        <ul>
+          {eletros.map((item) => (
+            <li key={item.id}>
+              <Link href={`/consumo/eletro-view/${item.id}`}>
+                <h2>{item.nome}</h2>
+                <figure>
+                  <Image
+                    src={item.img}
+                    alt={item.desc}
+                    width={300}
+                    height={300}
+                  />
+                  <figcaption>
+                    {item.desc} - <span>R$ {item.preco}</span>
+                  </figcaption>
+                </figure>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
