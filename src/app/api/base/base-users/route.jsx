6dee927dc22bf9cc5a47deaf45f3e44b5fb9ef0e
gutolive09 <gutolive01@gmail.com>
2023-11-09 11:09:02 -0300

    Api de login ajustada para cadastro.

--- src/app/api/base/base-users/route.jsx
+++ src/app/api/base/base-users/route.jsx
@@ -14,29 +14,45 @@ export async function GET(request,{params}){
 
 }
 
+const handleLogin = async (email, senha)=> {
+    const file = await fs.readFile(process.cwd() + "src/app/api/base/db.json", "utf8")
+
+    const dados = await JSON.parse(file)
+
+    try {
+        // for (let x =0; x< dados.usuarios.length; x++){
+        //     const userLista = lista.usuarios[x]
+        //     if(userLista.email == email && userLista.senha == senha){
+        //         return true
+        //     }
+        // }
+        const user = dados.usuarios.find((user)=> user.email == email && user.senha == senha);
+        return user
+
+    }catch(error){
+        console.log(error)
+    }
+
+}
+
 export async function POST(request, response){
-        //Criando uma chamada para acessar o arquivo JSON que foi criado!!!
-        const file =  await fs.readFile(process.cwd() + "/src/app/api/base/db.json","utf8");
+        
+    const {info,email,senha} = await request.json()
 
         //Recuperando a lista de usuários do arquivo JSON, realizando um
         //parse de arquivo para JSON.
-        const lista = await JSON.parse(file);
     
-        //Recebendo o request enviado como POST:
-        const userReq = await request.json();
+        switch (info){
 
-        try {
-            for (let x = 0; x < lista.usuarios.length; x++) {
-                const userLista = lista.usuarios[x];
-                //Realizando a validação do usuário:
-                if(userLista.email == userReq.email && userLista.senha == userReq.senha){
-                    //Retornando o status do request!
-                    return NextResponse.json({"status":true});
+            case "login":
+                const user = await handleLogin(email, senha);
+                if(user){
+                    return NextResponse.json({status:true, "user": user})
                 }
-            }
-        } catch (error) {
-            console.error(error);
+            case "cadastro":
+                return NextResponse.json({status:false})
+            default:
+                return NextResponse.json({status:false})
         }
-
-        return NextResponse.json({"status":false});
+    
 }
